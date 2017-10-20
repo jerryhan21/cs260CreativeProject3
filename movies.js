62eca514c126996eb7851bc08162cc07681611a8
astind <astindillon@gmail.com>
2017-10-20 00:56:31 +0000

    added stuff

---
+++ movies.js
@@ -0,0 +1,35 @@
+
+
+angular.module('app', [])
+  .controller('movieCtrl', movieCtrl);
+
+function movieCtrl ($scope, $http) {
+
+  $scope.movies = [];
+
+  $scope.addNew = function (movie) {
+    var moviePoster;
+    var movieTitle;
+    var movieYear;
+    var movieapi = 'http://www.theimdbapi.org/api/find/movie?title=';
+    movieapi += movie.title;
+    movieapi += '&year=';
+    movieapi += movie.year;
+    $http.get(movieapi).success(function(response) {
+	console.log(response);
+        $scope.movies.push({
+	   poster: response[0].poster.thumb,
+	   title: response[0].title,
+	   year: response[0].year
+        });
+    })
+    .error(function() {
+	console.log("jsons Error");
+    });
+
+    movie.title = '';
+    movie.year = '';
+
+  };
+}
+
